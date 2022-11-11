0e8efc47a948973672c9db1d125896257be8ad1c
selamvv <54860535+selamvv@users.noreply.github.com>
2022-11-11 12:01:07 -0500

    Update rock paper.py

--- rock paper.py
+++ rock paper.py
@@ -2,88 +2,117 @@
 ##Comp Science Lab_07
 ##2/15/19
 import random
-play= input("Do you want to play with the computer? ")
+play= input("Do you want to play with the computer? Type yes to play with the computer or no to play with another player")
 if play== ('yes'):
-    choice= (input('rock paper scissor lizard spock' ))
+    choice= (input('Your options are rock paper scissor lizard spock, enter a selectiom' ))
     computer=(random.randint(1,5))
     if computer==1:
         computer='rock'
+        print('computer selection: rock')
     elif computer==2:
         compter='scissors'
+        print('computer selection: scissors')
     elif computer==3:
         computer='paper'
+         print('computer selection: paper')
     elif computer==4:
         computer='spock'
+        print('computer selection: spock')
     else:
         computer='lizard'
+         print('computer selection: lizard')
+        
 
     if (choice == 'rock' and computer == 'scissors'):
-        print ("you win.")
+        print('your selction: rock <br> computer selection: scissors')
+        print ("You win!")
 
     elif (choice == 'rock' and computer == 'rock'):
+        print('your selction: rock <br> computer selection: rock')
         print ("Tie")
 
     elif (choice == 'scissors' and computer == 'paper'):
-        print ("you win.")
+        print('your selction: scissors <br> computer selection: paper')
+        print ("You win!")
 
     elif (choice == 'scissors' and computer == 'scissors'):
+        print('your selction: scissors <br> computer selection: scissors')
         print ("Tie")
         
     elif (choice == 'spock' and computer == 'spock'):
+        print('your selction: spock <br> computer selection: spock')
         print ("Tie")
         
     elif (choice == 'paper' and computer == 'paper'):
+        print('your selction: paper <br> computer selection: paper')
         print ("Tie")
         
     elif (choice == 'lizard' and computer == 'lizard'):
+        print('your selction: lizard <br> computer selection: lizard')
         print ("Tie")
         
     elif (choice == 'paper' and computer == 'scissors'):
+        print('your selction: paper <br> computer selection: scissors')
         print ("computer wins.")
 
-    elif (choice == 'rock'and computer == 'paper'):
+    elif (choice == 'rock' and computer == 'paper'):
+        print('your selction: rock <br> computer selection: paper')
         print ("computer wins.")
 
     elif (choice == 'paper' and computer == 'rock'):
+        print('your selction: paper <br> computer selection: rock')
         print ("computer wins.")
 
     elif (choice == 'scissors' and computer == 'rock'):
+        print('your selction: scissors <br> computer selection: rock')
         print ("computer wins.")
 
     elif (choice == 'rock' and computer == 'lizard'):
+        print('your selction: scissors <br> computer selection: rock')
         print ("you win.")
         
     elif (choice == 'lizard' and computer == 'rock'):
+        print('your selction: lizard <br> computer selection: rock')
         print ("computer wins.")
         
     elif (choice == 'spock' and computer == 'scissors'):
+        print('your selction: spock <br> computer selection: scissors')
         print ("you win.")
         
     elif (choice == 'scissors' and computer == 'spock'):
+        print('your selction: scissors <br> computer selection: spock')
         print ("computer wins.")
         
     elif (choice == 'scissors' and computer == 'lizard'):
+        print('your selction: scissors <br> computer selection: lizard')
         print ("computer wins.")
         
     elif (choice == 'lizard' and computer == 'scissors'):
+        print('your selction: lizard <br> computer selection: scissors')
         print ("you win.")
         
     elif (choice == 'lizard' and computer == 'paper'):
+        print('your selction: lizard <br> computer selection: paper')
         print ("you win.")
         
     elif (choice == 'paper' and computer == 'lizard'):
+        print('your selction: paper <br> computer selection: lizard')
         print ("computer wins.")
 
     elif (choice == 'paper' and computer == 'spock'):
+        print('your selction: paper <br> computer selection: spock')
         print ("you win.")
         
     elif (choice == 'spock' and computer == 'paper'):
+        print('your selction: spock <br> computer selection: paper')
         print ("computer wins.")
 
     elif (choice == 'spock' and computer == 'rock'):
+        print('your selction: spock <br> computer selection: rock')
         print ("you win.")
         
     elif (choice == 'rock' and computer == 'spock'):
+        print('your selction: rock <br> computer selection: spock')
         print ("computer wins.")
         
         
